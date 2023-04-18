43fe36962022d93cb2f51a9f7bfdebaad9fdb006
ayyubnurumi <ayyubinvestment@gmail.com>
2023-04-18 23:27:23 +0700

    calculator b aja

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Calculator from './Calculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>,
+)
